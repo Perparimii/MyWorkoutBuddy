bf38eea1b1bbd3895001b09af7b6205c2626d376
Perparimii <perparimlatifi361@gmail.com>
2026-05-02 12:46:23 +0200

    feat: implemented CreateWorkout and GetWorkouts in WorkoutController

--- backend/MyWorkoutBuddyApi/Controllers/WorkoutController.cs
+++ backend/MyWorkoutBuddyApi/Controllers/WorkoutController.cs
@@ -1,12 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyWorkoutBuddyApi.Models.DTOs;
+using MyWorkoutBuddyApi.Models.Entities;
+using MyWorkoutBuddyApi.Services;
 
 namespace MyWorkoutBuddyApi.Controllers
-{
+{ 
+    [ApiController]
+    [Route("api/[controller]")]
     public class WorkoutController : Controller
     {
-        public IActionResult Index()
+        private readonly IWorkoutService _workoutService;
+
+        public WorkoutController(IWorkoutService workoutService) 
         {
-            return View();
+            _workoutService = workoutService;
         }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Workout>> CreateWorkout(WorkoutDto newWorkout)
+        {
+            var workout = await _workoutService.CreateWorkoutAsync(newWorkout);
+
+            if(workout == null)
+            {
+                return BadRequest("Please create a valid workout!");
+            }
+
+            return Ok(workout);
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<WorkoutDto>>> GetWorkouts()
+        {
+            var workouts = await _workoutService.GetWorkoutsAsync();
+
+            if(workouts == null)
+            {
+                return NotFound("There are currently no existing workouts!");
+            }
+
+            return Ok(workouts);
+        }
+
     }
 }
