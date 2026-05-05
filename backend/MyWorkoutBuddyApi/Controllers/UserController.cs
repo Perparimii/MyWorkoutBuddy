c1e3878815d10aab528520005c8949015e7f2ef4
Perparimii <perparimlatifi361@gmail.com>
2026-05-05 22:12:13 +0200

    feat: created UserController for actions that normal users can do

---
+++ backend/MyWorkoutBuddyApi/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWorkoutBuddyApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
