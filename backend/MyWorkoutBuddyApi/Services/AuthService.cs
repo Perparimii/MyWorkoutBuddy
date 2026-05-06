15146dfda15acbee4b02ba24eb5c781b30ee29bf
Perparimii <perparimlatifi361@gmail.com>
2026-05-06 22:08:15 +0200

    feat: added user ID to claims in auth service for data filtering purposes

--- backend/MyWorkoutBuddyApi/Services/AuthService.cs
+++ backend/MyWorkoutBuddyApi/Services/AuthService.cs
@@ -29,6 +29,7 @@ namespace MyWorkoutBuddyApi.Services
                 
             var claims = new[]
             {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                  new Claim(ClaimTypes.Name, user.UserName),
                  new Claim(ClaimTypes.Role, user.Role)
              };
