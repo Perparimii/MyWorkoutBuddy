1b6dba85087f6646353efba1c982080adf41f521
Perparimii <perparimlatifi361@gmail.com>
2026-08-09 16:47:33 +0200

    feat: completed the form for login, only thing left to do is connect it to the backend

--- frontend/src/Pages/Landing.tsx
+++ frontend/src/Pages/Landing.tsx
@@ -1,13 +1,26 @@
-import { Card } from "@/components/ui/card";
+import { Card } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { useState, type SubmitEventHandler } from "react"
 
 function Landing(){
 
+    const[username, setUsername]= useState("");
+    const[password, setPassword]= useState("");
+    const[email, setEmail]= useState("");
+
+// const handleSubmit = (e: SubmitEventHandler<HTMLFormElement>) => {
+
+       
+
+
+
 return(
-<main className="min-h-screen grid grid-cols-2">
+    <main className="min-h-screen grid grid-cols-2">
 
-    {/*Left side*/}
+     {/*Left side*/}
 
-     <section className="flex items-center justify-center">
+         <section className="flex items-center justify-center">
                 <div>
                     <h1 className="text-5xl font-bold">
                         Welcome to MyWorkoutBuddy
@@ -21,7 +34,7 @@ return(
 
     {/*Right side*/} 
 
-    <section className="flex items-center justify-center">
+        <section className="flex items-center justify-center">
                 <Card className="w-[400px] p-6">
                     <h2 className="text-2xl font-bold">
                         Login
@@ -30,6 +43,38 @@ return(
                     <p className="mt-2 text-muted-foreground">
                         Login to access your workouts.
                     </p>
+                    <div className="mt-6 space-y-4">
+                      <form className="mt-6 space-y-4" onSubmit={(e) => {
+                         e.preventDefault();
+
+                         console.log("Username:", username);
+                         console.log("Password:", password);
+                         }}>
+
+                        <Input 
+                            type="text"
+                            placeholder="okto@pokto.com"
+                            value={email}
+                            onChange={(e)=> setEmail(e.target.value)}/>
+
+                        <Input 
+                            type="text"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e)=> setUsername(e.target.value)}/>
+
+                        <Input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e)=> setPassword(e.target.value)}/>
+                    
+
+                        <Button className="w-full" type="submit">
+                            Log In
+                        </Button>
+                      </form>
+                    </div>
 
                     {/* Form will go here */}
                 </Card>
