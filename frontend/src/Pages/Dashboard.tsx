bc48d1f675d85e7f5064ceebc9bbe6ae9c47a26c
Perparimii <perparimlatifi361@gmail.com>
2026-08-11 14:13:39 +0200

    feat: added logout functionality to the dashboard

--- frontend/src/Pages/Dashboard.tsx
+++ frontend/src/Pages/Dashboard.tsx
@@ -1,6 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+
 function Dashboard(){
 
-    return <h1>This is the Dashboard</h1>;
+    const navigate = useNavigate();
+
+    function handleLogOut(){
+        console.log("Button clicked");
+
+        localStorage.removeItem("token");
+
+        navigate("/");
+    }
+
+    return( 
+
+        <section className="flex items-center">
+            <div>
+                <h1>This is the dashboard</h1>
+
+                <Button className="w-full" onClick={handleLogOut}>Log out</Button>
+            </div>
+        </section>
+    
+    
+    )
+
 }
 
 export default Dashboard;
