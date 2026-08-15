64151c5b124a0f5c2f26fe9e0793130105ab151a
Perparimii <perparimlatifi361@gmail.com>
2026-08-15 13:03:16 +0200

    refactor: created dashboardSidebar custom component to hide the ugly code from the Dashboard page

---
+++ frontend/src/Components/customComponents/dashboardSidebar.tsx
@@ -0,0 +1,58 @@
+import { SidebarProvider, Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem, } from "../ui/sidebar";
+import { useNavigate } from "react-router-dom";
+
+
+
+function DashboardSidebar(){
+
+    const navigate = useNavigate();
+
+    function handleLogOut(){
+        console.log("Button clicked");
+
+        localStorage.removeItem("token");
+
+        navigate("/");
+    }
+
+    return(
+            <SidebarProvider>
+                    <Sidebar>
+                        <SidebarContent className="flex-1">
+                            <SidebarGroup>
+                                <SidebarGroupContent>
+                                    <SidebarMenu className="gap-3">
+                                        <SidebarMenuItem>
+                                            <SidebarMenuButton onClick={() => navigate("/home") } className="border border-blue-200 rounded-md hover:bg-blue-50 hover:border-blue-300">
+                                                Home
+                                            </SidebarMenuButton>
+                                        </SidebarMenuItem>
+                                        <SidebarMenuItem>
+                                            <SidebarMenuButton onClick={() => navigate("/plans") } className="border border-blue-200 rounded-md hover:bg-blue-50 hover:border-blue-300">
+                                                Plans
+                                            </SidebarMenuButton>
+                                        </SidebarMenuItem>
+                                        <SidebarMenuItem>
+                                            <SidebarMenuButton onClick={() => navigate("/myworkouts") } className="border border-blue-200 rounded-md hover:bg-blue-50 hover:border-blue-300">
+                                                My Workouts
+                                            </SidebarMenuButton>
+                                        </SidebarMenuItem>
+                                    </SidebarMenu>
+                                </SidebarGroupContent>
+                            </SidebarGroup>
+                        </SidebarContent>
+                        <SidebarFooter className="p-10 border-t border-blue-100 p-3 gap-2">
+                                    <SidebarMenu>
+                                        <SidebarMenuItem>
+                                            <SidebarMenuButton className="border border-red-200 rounded-md hover:bg-red-50 hover:border-red-300" onClick={handleLogOut}>
+                                                Log out
+                                            </SidebarMenuButton>
+                                        </SidebarMenuItem>
+                                    </SidebarMenu>
+                                </SidebarFooter>
+                    </Sidebar>
+                </SidebarProvider>
+    )
+}
+
+export {DashboardSidebar};
--- frontend/src/Pages/Dashboard.tsx
+++ frontend/src/Pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 //import { Button } from "@/components/ui/button";
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider } from "@/components/ui/sidebar";
+//import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider } from "@/components/ui/sidebar";
 import { useNavigate } from "react-router-dom";
+import { DashboardSidebar } from "@/components/customComponents/dashboardSidebar"
 
 
 function Dashboard(){
@@ -15,48 +16,14 @@ function Dashboard(){
         navigate("/");
     }
 
+
     return( 
     
     <main className="mmin-h-screen grid grid-cols-[250px_1fr]">
 
         <section className="flex items-center justify-center">
             <div>
-                <SidebarProvider>
-                    <Sidebar>
-                        <SidebarContent className="flex-1">
-                            <SidebarGroup>
-                                <SidebarGroupContent>
-                                    <SidebarMenu className="gap-3">
-                                        <SidebarMenuItem>
-                                            <SidebarMenuButton className="border border-blue-200 rounded-md hover:bg-...">
-                                                Home
-                                            </SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                        <SidebarMenuItem>
-                                            <SidebarMenuButton className="border border-blue-200 rounded-md hover:bg-blue-50 hover:border-blue-300">
-                                                Plans
-                                            </SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                        <SidebarMenuItem>
-                                            <SidebarMenuButton className="border border-blue-200 rounded-md hover:bg-blue-50 hover:border-blue-300">
-                                                My Workouts
-                                            </SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                    </SidebarMenu>
-                                </SidebarGroupContent>
-                            </SidebarGroup>
-                        </SidebarContent>
-                        <SidebarFooter className="p-10 border-t border-blue-100 p-3 gap-2">
-                                    <SidebarMenu>
-                                        <SidebarMenuItem>
-                                            <SidebarMenuButton className="border border-red-200 rounded-md hover:bg-red-50 hover:border-red-300" onClick={handleLogOut}>
-                                                Log out
-                                            </SidebarMenuButton>
-                                        </SidebarMenuItem>
-                                    </SidebarMenu>
-                                </SidebarFooter>
-                    </Sidebar>
-                </SidebarProvider>
+                <DashboardSidebar></DashboardSidebar>
             </div>
         </section>
 
