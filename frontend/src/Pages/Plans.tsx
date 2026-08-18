f4f674b4feb67a6e62e487ea16e939d0e3539724
Perparimii <perparimlatifi361@gmail.com>
2026-08-18 19:25:49 +0200

    feat: added Item, will be used for displaying the available plans, added an example to the plans page

--- frontend/src/Pages/Plans.tsx
+++ frontend/src/Pages/Plans.tsx
@@ -1,4 +1,7 @@
 import { DashboardSidebar } from "@/components/customComponents/dashboardSidebar";
+import { Item, ItemActions, ItemContent, ItemDescription, ItemMedia, ItemTitle } from "@/components/ui/item";
+import { Button } from "@/components/ui/button";
+//import { Icon } from "lucide-react";
 
 function Plans(){
 
@@ -7,6 +10,22 @@ return(
         <section className="flex items-center justify-center">
             <div>
                 <DashboardSidebar></DashboardSidebar>
+            </div>
+        </section>
+
+        <section className="flex flex-col items-center justify-center w-full">
+            <div>
+                <Item className="border border-blue-200 rounded-md hover:bg-blue-50 hover:border-blue-300">
+                    <ItemContent>
+                        <ItemTitle>Example plan</ItemTitle>
+                        <ItemDescription>
+                            Example descritption for the example plan to show the example blablabla
+                        </ItemDescription>
+                    </ItemContent>
+                    <ItemActions>
+                        <Button>Select</Button>
+                    </ItemActions>
+                </Item>
             </div>
         </section>
     </main>
