6e4974102fedfa13250d85843ad8acf154140a55
Perparimii <perparimlatifi361@gmail.com>
2026-08-25 20:19:32 +0200

    feat: implemented GetUsers changeUserRole and deleteUser for the admin services

--- backend/MyWorkoutBuddyApi/Services/AdminService.cs
+++ backend/MyWorkoutBuddyApi/Services/AdminService.cs
@@ -1,6 +1,65 @@
-﻿namespace MyWorkoutBuddyApi.Services
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyWorkoutBuddyApi.Data;
+using MyWorkoutBuddyApi.Models.DTOs;
+
+namespace MyWorkoutBuddyApi.Services
 {
-    public class AdminService
+    public class AdminService : IAdminService
     {
+        private readonly WorkoutDbContext _context;
+
+        public AdminService(WorkoutDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<UserDto>> GetUsers()
+        {
+            var users = await _context.Users.Select(e => new UserDto
+            {
+                Id = e.Id,
+                UserName = e.UserName,
+                Email = e.Email,
+                Role = e.Role,
+                PlanId = e.PlanId
+            }).ToListAsync();
+
+            return users;
+        }
+
+
+        public async Task<bool> ChangeUserRole(int id, String role)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if(user == null)
+            {
+                return false;
+            }
+
+            user.Role = role;
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+
+        public async Task<bool> DeleteUsers(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if(user == null)
+            {
+                return false;
+            }
+
+            _context.Users.Remove(user);
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
     }
 }
--- backend/MyWorkoutBuddyApi/Services/IAdminService.cs
+++ backend/MyWorkoutBuddyApi/Services/IAdminService.cs
@@ -1,6 +1,11 @@
-﻿namespace MyWorkoutBuddyApi.Services
+﻿using MyWorkoutBuddyApi.Models.DTOs;
+
+namespace MyWorkoutBuddyApi.Services
 {
     public interface IAdminService
     {
+        Task<IEnumerable<UserDto>> GetUsers();
+        Task<bool> ChangeUserRole(int id, String role);
+        Task<bool> DeleteUser(int id);
     }
 }
