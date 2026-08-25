fc764edc888c1df8d1cfb740d70b4b672b672c8a
Perparimii <perparimlatifi361@gmail.com>
2026-08-25 17:02:41 +0200

    feat: added UserService and interface as well as userDto

---
+++ backend/MyWorkoutBuddyApi/Models/DTOs/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using MyWorkoutBuddyApi.Models.Entities;
+
+namespace MyWorkoutBuddyApi.Models.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; } = "User";
+        public int PlanId { get; set; }
+        public WorkoutPlan? WorkoutPlan { get; set; }
+    }
+}
--- backend/MyWorkoutBuddyApi/Program.cs
+++ backend/MyWorkoutBuddyApi/Program.cs
@@ -44,6 +44,7 @@ builder.Services.AddScoped<IPlanService, PlanService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IExerciseService, ExerciseService>();
 builder.Services.AddScoped<IWorkoutService, WorkoutService>();
+builder.Services.AddScoped<IUserservice, UserService>();
 builder.Services.AddDbContext<WorkoutDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MyWorkoutDatabase")));
 
