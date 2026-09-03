3e7ceabb0b940548f9cc50feba1e431db5f5e75d
Perparimii <perparimlatifi361@gmail.com>
2026-09-03 23:15:16 +0200

    feat: added the needed interfaces for TodaysWorkout

--- frontend/src/Pages/TodaysWorkout.tsx
+++ frontend/src/Pages/TodaysWorkout.tsx
@@ -2,6 +2,23 @@ import { DashboardSidebar } from "@/components/customComponents/dashboardSidebar
 
 function TodaysWorkout(){
 
+    //Interfaces:
+    
+    interface Exercise {
+    name: string;
+    WarmupSets: number;
+    WorkingSets: number;
+    MinReps: number;
+    MaxReps: number;
+    }
+
+    interface TodaysWorkout{
+        WorkoutName: string;
+        DayOfWeek: number,
+        Exercises: Exercise[]
+    }   
+
+
 return(
     <main className="mmin-h-screen grid grid-cols-[250px_1fr]">
         <section className="flex items-center justify-center">
