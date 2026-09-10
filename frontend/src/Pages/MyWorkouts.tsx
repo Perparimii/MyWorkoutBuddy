935d72f76c55d66b5060c4b1cf650679da67c457
Perparimii <perparimlatifi361@gmail.com>
2026-09-10 20:05:10 +0200

    feat: added workout interface and fetch method for fetching workouts for My Workouts page

--- frontend/src/Pages/MyWorkouts.tsx
+++ frontend/src/Pages/MyWorkouts.tsx
@@ -1,6 +1,30 @@
 import { DashboardSidebar } from "@/components/customComponents/dashboardSidebar";
+import { useState, useEffect } from "react";
 
 function MyWorkouts(){
+
+interface Workout{
+    Name: string,
+    DayOfWeek: number,
+    ExerciseNumber: number,
+    PlanId: number
+}
+
+const[workouts, setWorkouts] = useState<Workout[]>([]);
+
+useEffect(() => {
+    const getWorkouts = async () => {
+        const response = await fetch("https://localhost:7027/api/Workout");
+
+        const data = await response.json();
+
+        setWorkouts(data);
+    }
+
+    getWorkouts();
+
+} ,[])
+
 
 return(
     <main className="mmin-h-screen grid grid-cols-[250px_1fr]">
@@ -9,6 +33,13 @@ return(
                 <DashboardSidebar></DashboardSidebar>
             </div>
         </section>
+
+        <section className="flex flex-col items-center justify-center w-full">
+            <div className="w-full max-w-4xl mx-auto px-6 space-y-4">
+
+            </div>
+        </section>
+
     </main>
 );
 
