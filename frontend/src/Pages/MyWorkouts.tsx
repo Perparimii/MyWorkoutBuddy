441606a0c330f8e222b05e5900ffc9e0d3f8ecdf
Perparimii <perparimlatifi361@gmail.com>
2026-09-10 20:21:02 +0200

    feat: succesfully implemented workout page

--- frontend/src/Pages/MyWorkouts.tsx
+++ frontend/src/Pages/MyWorkouts.tsx
@@ -1,20 +1,26 @@
 import { DashboardSidebar } from "@/components/customComponents/dashboardSidebar";
+import { Item, ItemContent, ItemDescription, ItemTitle } from "@/components/ui/item";
 import { useState, useEffect } from "react";
 
 function MyWorkouts(){
 
 interface Workout{
-    Name: string,
-    DayOfWeek: number,
-    ExerciseNumber: number,
-    PlanId: number
+    name: string,
+    dayOfWeek: number,
+    exerciseNumber: number,
+    planId: number
 }
 
 const[workouts, setWorkouts] = useState<Workout[]>([]);
 
 useEffect(() => {
     const getWorkouts = async () => {
-        const response = await fetch("https://localhost:7027/api/Workout");
+        const response = await fetch(`https://localhost:7027/api/Workout`,{
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+        }
+    });
 
         const data = await response.json();
 
@@ -35,8 +41,17 @@ return(
         </section>
 
         <section className="flex flex-col items-center justify-center w-full">
-            <div className="w-full max-w-4xl mx-auto px-6 space-y-4">
-
+            <div  className="w-full max-w-4xl mx-auto px-6 space-y-4">
+                {workouts.map((Workout) =>(
+                    <Item className="border border-blue-200 rounded-md hover:bg-blue-50 hover:border-blue-300">
+                        <ItemContent>
+                            <ItemTitle>{Workout.name}</ItemTitle>
+                            <ItemDescription>
+                                Day: {Workout.dayOfWeek} {" "} Exercise number: {Workout.exerciseNumber}
+                            </ItemDescription>
+                        </ItemContent>
+                    </Item>
+                ))}
             </div>
         </section>
 
