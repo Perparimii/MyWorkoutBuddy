65980f452e7296304c01b5987ab76abdf9221edc
Perparimii <perparimlatifi361@gmail.com>
2026-09-14 16:11:32 +0200

    feat: started fixing the dashboard, added needed fetches

--- frontend/src/Pages/Dashboard.tsx
+++ frontend/src/Pages/Dashboard.tsx
@@ -2,9 +2,64 @@
 //import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider } from "@/components/ui/sidebar";
 //import { useNavigate } from "react-router-dom";
 import { DashboardSidebar } from "@/components/customComponents/dashboardSidebar"
+import { useEffect, useState } from "react";
 
 
 function Dashboard(){
+    interface Plan {
+    id: number;
+    name: string;
+    description: string;
+    format: string
+    }
+    interface UserProfile {
+    id: number;
+    userName: string;
+    email: string;
+    planId: number | null;
+}
+    const [plan, setPlan] = useState<Plan | null>(null);
+    const [user, setUser] = useState<UserProfile | null>(null);
+
+    useEffect(()=> {
+
+    const getPlan = async ()=> {
+
+        const planId = user?.planId;
+
+        const response = await fetch(`https://localhost:7027/api/Plans/${planId}`,{
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+        }
+    });
+
+        const data = await response.json();
+
+        setPlan(data);
+
+    };
+
+    getPlan();
+
+    const getUser = async () =>{
+        const response = await fetch(`https://localhost:7027/api/User`,{
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+        }
+    });
+
+    const data = await response.json();
+
+    setUser(data);
+    };
+
+    getUser();
+
+    }, []);
+
+        
 
     return( 
     
